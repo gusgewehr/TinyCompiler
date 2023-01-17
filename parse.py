d6dafcb6ae4b948fd48811ecb4cedb7b4222947a
GUSTAVO GEWEHR SOARES <52550134+gusgewehr@users.noreply.github.com>
2023-01-17 08:52:32 -0300

    Parte 2 do Teeny Tiny Compiler finalizada
    https://austinhenley.com/blog/teenytinycompiler2.html

--- parse.py
+++ parse.py
@@ -6,10 +6,14 @@ class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
 
+        self.symbols = set()    # Variables declared so far.
+        self.labelsDeclared = set() # Labels declared so far.
+        self.labelsGotoed = set() # Labels goto'ed so far.
+
         self.curToken = None
         self.peekToken = None
         self.nextToken()
-        self.nextToken()    # Call this twice to initialize current and peek.
+        self.nextToken()     # Call this twice to initialize current and peek.
 
     # Return true if the current token matches.
     def checkToken(self, kind):
@@ -33,6 +37,10 @@ class Parser:
 
     def abort(self, message):
         sys.exit("Error. " + message)
+
+    # Return true if the current token is a comparison operator.
+    def isComparisonOperator(self):
+        return self.checkToken(TokenType.GT) or self.checkToken(TokenType.GTEQ) or self.checkToken(TokenType.LT) or self.checkToken(TokenType.LTEQ) or self.checkToken(TokenType.EQEQ) or self.checkToken(TokenType.NOTEQ)
     
     # Production rules.
 
@@ -60,7 +68,12 @@ class Parser:
         while not self.checkToken(TokenType.EOF):
             self.statement()
 
-        # One of the following statements...
+        # Check that each label referenced in a GOTO is declared.
+        for label in self.labelsGotoed:
+            if label not in self.labelsDeclared:
+                self.abort("Attempting to GOTO to undeclared label: " + label)
+
+    # One of the following statements...
     def statement(self):
         # Check the first token to see what kind of statement this is.
 
@@ -111,26 +124,42 @@ class Parser:
         elif self.checkToken(TokenType.LABEL):
             print("STATEMENT-LABEL")
             self.nextToken()
+
+            # Make sure this label doesn't already exist.
+            if self.curToken.text in self.labelsDeclared:
+                self.abort("Label already exists: " + self.curToken.text)
+            self.labelsDeclared.add(self.curToken.text)
+
             self.match(TokenType.IDENT)
 
         # "GOTO" ident
         elif self.checkToken(TokenType.GOTO):
             print("STATEMENT-GOTO")
             self.nextToken()
+            self.labelsGotoed.add(self.curToken.text)
             self.match(TokenType.IDENT)
 
         # "LET" ident "=" expression
         elif self.checkToken(TokenType.LET):
-            print("STATEMENT-LET")
             self.nextToken()
+
+            #  Check if ident exists in symbol table. If not, declare it.
+            if self.curToken.text not in self.symbols:
+                self.symbols.add(self.curToken.text)
+
             self.match(TokenType.IDENT)
             self.match(TokenType.EQ)
+            
             self.expression()
 
         # "INPUT" ident
         elif self.checkToken(TokenType.INPUT):
-            print("STATEMENT-INPUT")
             self.nextToken()
+
+            #If variable doesn't already exist, declare it.
+            if self.curToken.text not in self.symbols:
+                self.symbols.add(self.curToken.text)
+
             self.match(TokenType.IDENT)
 
         # This is not a valid statement. Error!
@@ -139,3 +168,65 @@ class Parser:
 
         # Newline.
         self.nl()
+
+    # comparison ::= expression (("==" | "!=" | ">" | ">=" | "<" | "<=") expression)+
+    def comparison(self):
+        print("COMPARISON")
+
+        self.expression()
+        # Must be at least one comparison operator and another expression.
+        if self.isComparisonOperator():
+            self.nextToken()
+            self.expression()
+        else:
+            self.abort("Expected comparison operator at: " + self.curToken.text)
+
+        # Can have 0 or more comparison operator and expressions.
+        while self.isComparisonOperator():
+            self.nextToken()
+            self.expression()
+
+    # expression ::= term {( "-" | "+" ) term}
+    def expression(self):
+        print("EXPRESSION")
+
+        self.term()
+        # Can have 0 or more +/- and expressions.
+        while self.checkToken(TokenType.PLUS) or self.checkToken(TokenType.MINUS):
+            self.nextToken()
+            self.term()
+
+    # term ::= unary {( "/" | "*" ) unary}
+    def term(self):
+        print("TERM")
+
+        self.unary()
+        # Can have 0 or more *// and expressions.
+        while self.checkToken(TokenType.ASTERISK) or self.checkToken(TokenType.SLASH):
+            self.nextToken()
+            self.unary()
+
+
+    # unary ::= ["+" | "-"] primary
+    def unary(self):
+        print("UNARY")
+
+        # Optional unary +/-
+        if self.checkToken(TokenType.PLUS) or self.checkToken(TokenType.MINUS):
+            self.nextToken()        
+        self.primary()
+    
+    # primary ::= number | ident
+    def primary(self):
+        print("PRIMARY (" + self.curToken.text + ")")
+
+        if self.checkToken(TokenType.NUMBER): 
+            self.nextToken()
+        elif self.checkToken(TokenType.IDENT):
+            # Ensure the variable already exists.
+            if self.curToken.text not in self.symbols:
+                self.abort("Referencing variable before assignment: " + self.curToken.text)
+            self.nextToken()
+        else:
+            # Error!
+            self.abort("Unexpected token at " + self.curToken.text)
